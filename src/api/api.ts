5945290f94be35464df55571aa1973c434e348f8
Kadir Karadavut <kadir9625@gmail.com>
2023-08-18 16:49:58 +0200

    add correct api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://kadir-todo-2b671e510570.herokuapp.com/',
+  baseURL: 'https://kadir-todo-api-b4bb4553125e.herokuapp.com',
 })
 
 export default api
